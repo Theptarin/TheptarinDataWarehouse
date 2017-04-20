534a8c177481de48aba8dffe66dc7feaa90b8b7c
suchartbu <suchartbu@gmail.com>
2017-04-20 18:36:39 +0700

    version 20/04/56

---
+++ transfer/hims_transfer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /var/www/service/TheptarinDataWarehouse/transfer/
+sh Db2MySQL.sh patient
+sh Db2MySQL.sh lab_request
---
+++ transfer/hims_transfer_minutely.sh
@@ -0,0 +1,7 @@
+#Save UTF8 Only Don't save TIS620
+cd /home/administrator/it_programs/transfer
+sh mysql@lab_income_today.sh
+sh mysql@patient_card_status3.sh
+sh mysql@patient_card_status1.sh
+sh mysql@patient_card_status4.sh
+
